04f5402a2402c8b6d24f19432a6b71d20c8b3daa
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-12-28 22:50:29 -0500

    ✨Finished Navbar/Sidebar
    Finished responsive Navbar and Sidebar

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -10,29 +10,29 @@ import {
     SidebarRoute
 } from './SidebarElements';
 
-const SideBar = () => {
+const SideBar = ({isOpen, toggle}) => {
     return (
-        <SidebarContainer>
-            <Icon>
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
                 <CloseIcon/>
             </Icon>
             <SidebarWrapper>
                 <SidebarMenu>
-                    <SidebarLink to="about">
+                    <SidebarLink to="about" onClick={toggle}>
                         About
                     </SidebarLink>
-                    <SidebarLink to="discover">
+                    <SidebarLink to="discover" onClick={toggle}>
                         Discover
                     </SidebarLink>
-                    <SidebarLink to="services">
+                    <SidebarLink to="services" onClick={toggle}>
                         Services
                     </SidebarLink>
-                    <SidebarLink to="signup">
+                    <SidebarLink to="signup" onClick={toggle}>
                         Sign Up
                     </SidebarLink>
                 </SidebarMenu>
                 <SideBtnWrap>
-                    <SidebarRoute to="/signin">
+                    <SidebarRoute to="/signin" onClick={toggle}>
                         Sign In
                     </SidebarRoute>
                 </SideBtnWrap>
