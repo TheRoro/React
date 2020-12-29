40530e5340561f52f014650c6a76dd0eb476a001
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-12-28 22:43:36 -0500

    ✨Responsive Sidebar
    Created sidebar with styled components

---
+++ src/Components/Sidebar/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { 
+    SidebarContainer, 
+    Icon, 
+    CloseIcon,
+    SidebarWrapper,
+    SidebarMenu,
+    SidebarLink,
+    SideBtnWrap,
+    SidebarRoute
+} from './SidebarElements';
+
+const SideBar = () => {
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon/>
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">
+                        About
+                    </SidebarLink>
+                    <SidebarLink to="discover">
+                        Discover
+                    </SidebarLink>
+                    <SidebarLink to="services">
+                        Services
+                    </SidebarLink>
+                    <SidebarLink to="signup">
+                        Sign Up
+                    </SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to="/signin">
+                        Sign In
+                    </SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
+    )
+}
+
+export default SideBar;
